907ce41fddfe9356ec370649ba97e517e1d79c5b
karthikyeluripati <sy493@njit.edu>
2024-03-24 13:02:07 -0400

    Menu plugin added

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -1,7 +1,10 @@
 import sys
 from app.commands import Command
-
+import os
 
 class MenuCommand(Command):
     def execute(self):
-        print(f'Menu')
+        print("Available Plugins:")
+        plugin_files=[f for f in os.listdir("./app/plugins") if f != '__init__.py' and f != '__pycache__']
+        for plugin_file in plugin_files:
+                print(f"- {plugin_file}")
